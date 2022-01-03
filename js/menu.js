a670125fc78b3ebec98896090cf3663b19e70c49
Yuliia2991 <yuliiakurian@gmail.com>
2022-01-03 19:17:51 +0200

    Adds the adaptive menu to header

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,16 @@
-// (() => {
-//   const refs = {
-//     openMenuBtn: document.querySelector('[data-menu-open]'),
-//     closeMenuBtn: document.querySelector('[data-menu-close]'),
-//     menu: document.querySelector('[data-menu]'),
-//   };
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
 
-//   refs.openMenuBtn.addEventListener('click', toggleMenu);
-//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
-//   function toggleMenu() {
-//     refs.menu.classList.toggle('is-hidden');
-//   }
-// })();
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+})();
 
 
